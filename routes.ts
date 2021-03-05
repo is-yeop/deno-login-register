cc43e2d83d1210f088d6e105514e1a79b22a9020
is-yeop <yeop@yeop.kr>
2021-03-06 01:24:03 +0900

    Write home page with ejs
    dejs를 이용하여 ejs 파일을 이용한 페이지 렌더링

--- routes.ts
+++ routes.ts
@@ -1,7 +1,6 @@
 import {RouterContext} from "https://deno.land/x/oak/mod.ts";
+import {renderFileToString} from "https://deno.land/x/dejs/mod.ts";
 
-export const home = (ctx: RouterContext) => {
-    ctx.response.body = {
-        message: 'Welcome',
-    };
+export const home = async (ctx: RouterContext) => {
+    ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/home.ejs`, {});
 }
