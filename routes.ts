a76ba9de5e4a94152728ea850a28a5f12e735fbe
is-yeop <yeop@yeop.kr>
2021-03-06 06:53:35 +0900

    Write register page
    회원가입 페이지 오류를 뚫고 드디어 끝났다....
    router에서 post 주소를 잘못 적어둔 오류였다...

--- routes.ts
+++ routes.ts
@@ -1,6 +1,61 @@
 import {RouterContext} from "https://deno.land/x/oak/mod.ts";
 import {renderFileToString} from "https://deno.land/x/dejs/mod.ts";
+import {hashSync, compareSync} from "https://deno.land/x/bcrypt/mod.ts";
+import {users, User} from "./users.ts";
 
 export const home = async (ctx: RouterContext) => {
     ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/home.ejs`, {});
+}
+
+export const login = async (ctx: RouterContext) => {
+    ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/login.ejs`, {
+        error: 0
+    });
+}
+
+export const register = async (ctx: RouterContext) => {
+    ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/register.ejs`, {});
+}
+
+export const protectedRoute = async (ctx: RouterContext) => {
+    ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/protected.ejs`, {});
+}
+
+export const postLogin = async (ctx: RouterContext) => {
+    const value = await ctx.request.body().value;
+    const username = value.get('username');
+    const password = value.get('password');
+
+    const user = users.find((u: User)=> u.username === username)
+    if (!user) {
+        ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/login.ejs`, {
+            error: "Incorrect username"
+        });
+    } else if (!compareSync(password, user.password)) {
+        ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/login.ejs`, {
+            error: "Incorrect password"
+        });
+    } else {
+        console.log("Success");
+    }
+}
+
+export const postRegister = async (ctx: RouterContext) => {
+    const value = await ctx.request.body().value;
+    const name = value.get('name');
+    const username = value.get('username');
+    const password = value.get('password');
+
+    const hashedPassword = hashSync(password);
+    const user = {
+        name,
+        username,
+        password: hashedPassword,
+    };
+    users.push(user);
+    ctx.response.redirect('/login');
+}
+
+export const logout = async (ctx: RouterContext) => {
+    ctx.response.body = await renderFileToString(`${Deno.cwd()}/views/logout.ejs`, {});
 }
--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import {Application, Router} from "https://deno.land/x/oak/mod.ts";
-import {home} from "./routes.ts";
+import {home, login, register, protectedRoute, postLogin, postRegister, logout} from "./routes.ts";
 
 const PORT = 8000;
 
@@ -9,10 +9,21 @@ const router = new Router();
 
 router
     .get('/', home)
+    .get('/login', login)
+    .get('/register', register)
+    .get('/protected', protectedRoute)
+    .post('/login', postLogin)
+    .post('/register', postRegister)
+    .get('/logout', logout)
 ;
 
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+// Error 처리
+app.addEventListener('error', event => {
+   console.log(event.error);
+});
 
 app.listen({port: PORT});
 console.log("Server started on port: " + PORT);
